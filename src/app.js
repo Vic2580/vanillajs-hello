f570658027fcdfa70c75bfa4c352ea0e92665967
Vic2580 <115164142+Vic2580@users.noreply.github.com>
2024-08-03 19:16:34 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -4,8 +4,45 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import "./assets/img/excuses.jpg";
+
+let who = [
+  "The dog",
+  "My grandma",
+  "The mailman",
+  "My bird",
+  "My Mom",
+  "A random man"
+];
+let action = ["ate", "peed", "crushed", "broke", "set on fire"];
+let what = ["my homework", "my phone", "the car", "my house", "my computer"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+
+function getRandomNumber(a, b) {
+  return Math.floor(Math.random() * (b - a) + a);
+}
+function getRandom(anyArray) {
+  let max = anyArray.length;
+  let min = 0;
+  let random = getRandomNumber(min, max);
+  return anyArray[random];
+}
 
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML =
+    getRandom(who) +
+    " " +
+    getRandom(action) +
+    " " +
+    getRandom(what) +
+    " " +
+    getRandom(when);
 };
